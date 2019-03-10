615d85bb7a502d5b69128db60b2caf24fddbe59e
Smart developer <developer@yandex.ru>
2019-03-10 18:25:48 +0300

    Делаем шаблон для home page

--- tennis_blog.py
+++ tennis_blog.py
@@ -1,17 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+posts = [
+    {
+        'author': 'Rafa Nadal',
+        'title': 'One More Injury',
+        'content': 'Retired from Indian Wells and Miami tournaments. Sadness :(',
+        'date_posted': 'March 1, 2019'
+    },
+    {
+        'author': 'Maria Sharapova',
+        'title': 'My First Disqualification',
+        'content': 'Never again meldonium',
+        'date_posted': 'March 3, 2019'
+    }]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
-
-
-@app.route("/about")
-def about():
-    return "<h1>About Page</h1>"
+    return render_template('home_page.html', posts=posts)
 
 
 if __name__ == '__main__':
