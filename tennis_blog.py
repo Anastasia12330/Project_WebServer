71947dc0b3e39eb8ae7ae307e7a34e0fa0eecc18
Smart developer <developer@yandex.ru>
2019-03-16 21:33:35 +0300

    Поправили навигационную панель и добавили шаблон регистрации пользователей

--- tennis_blog.py
+++ tennis_blog.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'blog_for_tennis_players'
 posts = [
     {
         'author': 'Rafa Nadal',
@@ -16,14 +20,46 @@ posts = [
     }]
 
 
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home_page.html', posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template('about_page.html', title='About')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
